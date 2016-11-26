79bddf0b91d7b9eb5f1b83501194e2f801e04b94
Gustavo Ramos <gustavoramossantiago@gmail.com>
2016-11-26 10:26:32 -0500

    generate pages controller, modify routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+#Pages controllers contain all the code for any page inside /pages
+class PagesController < ApplicationController
+#All the back-end code for pages/index	
+  def index
+  end
+#All the back-end code for pages/home
+  def home
+  end
+#All the back-end code for pages/profile
+  def profile
+  end
+#All the back-end code for pages/explore
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+#Define Root URL
+root 'pages#index'
+
+#Define routes for pages, get means I want this page to be access as a web page.
+get '/profile' => 'pages#profile'
+
+get '/explore' => 'pages#explore'
+
+get '/home' => 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
