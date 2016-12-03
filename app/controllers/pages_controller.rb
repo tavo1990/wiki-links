4347e1fbee0d24dec912132ddd951bc8874395af
Gustavo Ramos <gustavoramossantiago@gmail.com>
2016-12-03 10:17:35 -0500

    Deleted explore view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,18 +5,18 @@ class PagesController < ApplicationController
   end
 #All the back-end code for pages/home
   def home
+    @posts = Post.all
   end
 #All the back-end code for pages/profile
   def profile
   	# Grab the username from the URL as :id
   	# if (User.find_by_username(params[:id]))
   		@username = params[:id]
+      @posts = Post.all
   	# else
   	# 	#Redirect to 404 (fot now)
   	# 	redirect_to root_path, :notice=> "User not found!"
   	# end
   end
-#All the back-end code for pages/explore
-  def explore
-  end
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validate :user_id, presence: true
-  validate :content, presence: true, length: (maximum: 140)
-  default_scope => {order(created_at: :desc)} #displays in order starting from the the most recent post
+  validates :user_id, presence: true
+  validates :content, length: { maximum: 140 }, presence: true
+  default_scope -> { order('created_at DESC') } #displays in order starting from the the most recent post
 end
 
